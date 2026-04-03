1b74b22d41ff89420a778ae0eff3e4ff242b5b8c
Alessandro <alllessandrogomes@gmail.com>
2026-04-03 18:17:46 -0300

    :sparkles:feat: add fallback products in case the api fail

--- app/utils/get-image-url.ts
+++ app/utils/get-image-url.ts
@@ -1,7 +1,15 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL || '';
 
 export const getImageUrl = (url: string) => {
   if (!url) return '';
-  if (url.startsWith('http')) return url;
+  if (
+    url.startsWith('http') ||
+    url.startsWith('blob:') ||
+    url.startsWith('data:')
+  )
+    return url;
+
+  if (!url.startsWith('/uploads')) return url;
+
   return `${API_URL}${url}`;
 };
